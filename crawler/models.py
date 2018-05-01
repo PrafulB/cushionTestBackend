8f219c92d3e0367251a999dc03aacdad4d7e0d20
prtsh32@gmail.com <pmb418@nyu.edu>
2018-04-30 23:23:58 -0400

    Added IP To Model, Added Status Code to View response

--- crawler/models.py
+++ crawler/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class HitStats(models.Model):
     """ Class To Track Stats of Hits Received """
     api = models.CharField(max_length=255, blank=False, unique=False)
+    userIP = models.CharField(max_length=255, blank=False, unique=False)
     requestTimestamp = models.DateTimeField(auto_now=True)
