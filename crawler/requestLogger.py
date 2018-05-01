4dd62543a3fb5db13c52eb25401874f3876da58d
prtsh32@gmail.com <pmb418@nyu.edu>
2018-04-30 23:52:01 -0400

    Added middleware functionality to log all requests to DB

--- crawler/requestLogger.py
+++ crawler/requestLogger.py
@@ -1,10 +1,17 @@
-import datetime
+from crawler.models import HitStats
+from django.utils import timezone
 
 class RequestLoggingMiddleware:
+    """ Class to log all received requests and add to DB """
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        print(request.get_host(), datetime.datetime.now().isoformat())
+        """
+        Function that logs the userIP and the timestamp to the db
+        """
+        req = HitStats(userIP=request.get_host(), requestTimestamp=timezone.now())
+        req.save()
         response = self.get_response(request)
         return response
