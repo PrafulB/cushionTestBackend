ac7070d27f38ffacefb1c62553211c21fb095d0f
prtsh32@gmail.com <pmb418@nyu.edu>
2018-05-01 01:56:22 -0400

    Added __str__ method to model for better readability of db contents

--- crawler/models.py
+++ crawler/models.py
@@ -4,3 +4,7 @@ class HitStats(models.Model):
     """ Class To Track Stats of Hits Received """
     userIP = models.CharField(max_length=255, blank=False, unique=False)
     requestTimestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """ Return a more readable string for the data contained """
+        return self.userIP + " " + str(self.requestTimestamp)
