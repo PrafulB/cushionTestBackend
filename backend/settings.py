236393f2ff71670938bac9d7cf00c2e442142ffb
prtsh32@gmail.com <pmb418@nyu.edu>
2018-05-01 12:41:14 -0400

    Changes for Heroku Deployment

--- backend/settings.py
+++ backend/settings.py
@@ -101,9 +101,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'static'),
+  os.path.join(BASE_DIR, 'crawler/static'),
 ]
 
 
